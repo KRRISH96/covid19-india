89a520db68dd19e085464481790f8ef046dac6ed
sai <kskprasad96@gmail.com>
2020-05-21 22:05:19 +0530

    Add data diff info

--- src/components/OverallData/index.jsx
+++ src/components/OverallData/index.jsx
@@ -2,24 +2,37 @@ import React from 'react';
 import './styles.scss';
 
 const OverallData = ({
- overallData: {confirmed, active, recovered, deaths} 
+ overallData: {
+   confirmed, active, recovered, deaths, deltaconfirmed, deltadeaths, deltarecovered
+  }
 }) => (
   <div className="overall-data">
     <div className="card">
       <span className="status">confirmed</span>
-      <span className="data">{confirmed || '-'}</span>
+      <span className="data">
+        <b>{confirmed || '-'}</b>
+        <small>+{deltaconfirmed}</small>
+      </span>
     </div>
     <div className="card">
       <span className="status">active</span>
-      <span className="data active">{active || '-'}</span>
+      <span className="data active">
+        <b>{active || '-'}</b>
+      </span>
     </div>
     <div className="card">
       <span className="status">recovered</span>
-      <span className="data recovered">{recovered || '-'}</span>
+      <span className="data recovered">
+        <b>{recovered || '-'}</b>
+        <small>+{deltarecovered}</small>
+      </span>
     </div>
     <div className="card">
       <span className="status">deceased</span>
-      <span className="data deceased">{deaths || '-'}</span>
+      <span className="data deceased">
+        <b>{deaths || '-'}</b>
+        <small>+{deltadeaths}</small>
+      </span>
     </div>
   </div>
 )
