0c2bcb40daf71546d28f7d80825fb22d186249fd
sai <kskprasad96@gmail.com>
2020-06-29 22:54:30 +0530

    Add period filter options

--- src/components/TimelineChart/index.jsx
+++ src/components/TimelineChart/index.jsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { LineChart, AreaChart } from 'react-chartkick'
 import 'chart.js'
 import './styles.scss';
 
+const TIMELINE_PERIODS ={
+  0: 'All',
+  7: "7 Days",
+  30: "30 Days",
+  60: "60 Days",
+  90: "90 Days",
+}
+
 const TimelineChart = ({timeline, chartFor='total', type="line"}) => {
+  const [activePeriod, setActivePeriod] = useState(30);
+
   let data = [
     { name: 'Confirmed', data: {} },
     { name: 'Recovered', data: {} },
@@ -34,9 +44,16 @@ const TimelineChart = ({timeline, chartFor='total', type="line"}) => {
   const ChartType = type === 'area' ? AreaChart : LineChart;
 
   return (
-    <div className="timeline-chart-wrapper">
-      <ChartType data={data} colors={colors} title={`${chartFor} cases`} />
-    </div>
+    <React.Fragment>
+      <ul className="period-list">
+        {Object.keys(TIMELINE_PERIODS).map(period => (
+          <li key={period} onClick={() => setActivePeriod(period)} className={`${Number(period) === Number(activePeriod) ? 'period-active' : ''}`}>{TIMELINE_PERIODS[period]}</li>
+        ))}
+      </ul>
+      <div className="timeline-chart-wrapper">
+        <ChartType data={data} colors={colors} title={`${chartFor} cases`} />
+      </div>
+    </React.Fragment>
   )
 }
 
